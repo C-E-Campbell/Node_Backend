b6593e49f0b3d18686dc73b21ff90e48671b6bec
Charles <codelyfewithcharlie@gmail.com>
2020-07-24 04:26:55 -0400

    updates

--- server/data.js
+++ server/data.js
@@ -1,19 +1,21 @@
-module.exports = [
-  {
-    id: 1,
-    image:
-      'https://res.cloudinary.com/dvxz6le8d/image/upload/v1595572195/Deathstalker_bloohn.jpg',
-    thumbnail:
-      'https://res.cloudinary.com/dvxz6le8d/image/upload/c_thumb,w_200,g_face/v1595572195/Deathstalker_bloohn.jpg',
-    name: 'Deathstalker',
-    species: 'Leiurus quinquestriatus',
-    poisonous: true,
-    aka: [
-      'Palestine yellow scorpion',
-      'Naqab desert scorpion',
-      'Omdurman scorpion',
-    ],
-    wiki: 'https://en.wikipedia.org/wiki/Deathstalker',
-    location: ['North Africa', 'Middle East'],
-  },
-];
+module.exports = {
+  data: [
+    {
+      id: 1,
+      image:
+        'https://res.cloudinary.com/dvxz6le8d/image/upload/v1595572195/Deathstalker_bloohn.jpg',
+      thumbnail:
+        'https://res.cloudinary.com/dvxz6le8d/image/upload/c_thumb,w_200,g_face/v1595572195/Deathstalker_bloohn.jpg',
+      name: 'Deathstalker',
+      species: 'Leiurus quinquestriatus',
+      poisonous: true,
+      aka: [
+        'Palestine yellow scorpion',
+        'Naqab desert scorpion',
+        'Omdurman scorpion',
+      ],
+      wiki: 'https://en.wikipedia.org/wiki/Deathstalker',
+      location: ['North Africa', 'Middle East'],
+    },
+  ],
+};
