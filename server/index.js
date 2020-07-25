ef25ed0e10a6065ac0dd89ec50ac5d653d2b2300
Charles <codelyfewithcharlie@gmail.com>
2020-07-25 04:30:00 -0400

    updates

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 require('dotenv').config({ path: '../.env' });
 const { PORT } = process.env;
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 const scorpionRouter = require('./routes/scorpionsRouter');
 const data = require('./data');
+
 const app = express();
 
+app.use(express.static(`${__dirname}/../build`));
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 
 app.use('/api/v1/scorpions', scorpionRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 app.listen(8293, () => {
   console.log(`Open on port 8293`);
